9ea573bb55d0c1bbac7b02549c7d525cd24c2ada
Kishan <kkpatel911@gmail.com>
2018-12-23 19:40:42 -0500

    added aditional info

--- app.js
+++ app.js
@@ -2,27 +2,39 @@ var toggleList = document.getElementById("toggleList");
 var listdiv = document.querySelector('.list');
 var descriptionInput = document.querySelector("input.description");
 var apple = document.getElementById("apple");
+var listul = listdiv.querySelector('ul');
 var descriptionButton = document.querySelector("button.description");
 var addItemInput = document.querySelector("input.addItemInput")
 var addItemButton = document.querySelector("button.addItemButton")
-var removeItemButton = document.querySelector("button.removeItemButton")
+
 // const listItems = document.getElementsByTagName('li');
 
-listdiv.addEventListener("mouseover", (event) => {
-  if (event.target.tagName == "LI") {
-    event.target.textContent = event.target.textContent.toUpperCase();
+listul.addEventListener("click", (event) => {
+  if (event.target.tagName == "BUTTON") {
+    if (event.target.className == 'remove') {
+      let li = event.target.parentNode;
+      let ul = li.parentNode;
+      ul.removeChild(li);
+    }
+    if (event.target.className == 'up') {
+      let li = event.target.parentNode;
+      let prevLi = li.previousElementSibling;
+      let ul = li.parentNode;
+      if (prevLi) {
+        ul.insertBefore(li, prevLi);
+      }
+      clas
+    }
+    if (event.target.className == 'down') {
+      let li = event.target.parentNode;
+      let nextLi = li.nextElementSibling;
+      let ul = li.parentNode;
+      if (nextLi) {
+        ul.insertBefore(nextLi, li);
+      }
+    }
   }
 });
-
-listdiv.addEventListener("mouseout", () => {
-  if (event.target.tagName == "LI") {
-    event.target.textContent = event.target.textContent.toLowerCase();
-  }
-});
-
-
-
-
 toggleList.addEventListener("click", () => {
   if (listdiv.style.display == "none") {
     listdiv.style.display = 'block';
@@ -45,13 +57,6 @@ addItemButton.addEventListener("click", () => {
   li.textContent = addItemInput.value;
   ul.appendChild(li);
   addItemInput.value = "";
-});
-
-removeItemButton.addEventListener("click", () => {
-  var ul = document.getElementsByTagName("ul")[0];
-  var li = document.querySelector('li:last-child');
-  ul.removeChild(li);
-
 });
 
 
